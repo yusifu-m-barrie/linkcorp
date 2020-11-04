fd6cea15796c0e9d399db9bc04118f461bb90bcb
Yusifu M. Barrie <34960874+yusifu-m-barrie@users.noreply.github.com>
2020-11-04 13:57:56 +0100

    adding receiver's email

--- contact.php
+++ contact.php
@@ -40,7 +40,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "example@yourdomain.com";
+$address = " contact@linkcorp-sl.com";
 
 
 // Configuration option.
